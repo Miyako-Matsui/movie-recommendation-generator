03dd5594ce0f642be8dd1d128602b9b6997dc5fd
Miyako Matsui <miyakomatsui18@gmail.com>
2023-04-08 15:55:18 +1200

    Edit home page

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -7,8 +7,12 @@ import Comments from './Comments'
 function Home() {
   return (
     <div>
+      <img
+        src="https://3.bp.blogspot.com/-9wNOOflM8GI/VMItkAskJbI/AAAAAAAAqvU/Bi14dEAI4UM/s800/line_movie.png"
+        alt="line"
+      ></img>
       <h1>Welcome!</h1>
-      <h2>Take me to the Movie!</h2>
+      <h2>💖 Take me to the Movie! 💖</h2>
       <Movies />
       <br />
       <Recommendations />
@@ -16,6 +20,10 @@ function Home() {
       <Comments />
       <br />
       <p>&copy;都</p>
+      <img
+        src="https://1.bp.blogspot.com/-aQcAUnRVScE/XL8GiDp1jTI/AAAAAAABSgk/DgdEnOnw6SU8PmYqG7r0nWVXFsoDEsLoQCLcBGAs/s800/animal_mizugi_set.png"
+        alt="line"
+      ></img>
     </div>
   )
 }
